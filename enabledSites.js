1d84956d2105f1677de94abc1061f9dd0d3d0b5e
8BitJonny <timon99@gmx.de>
2021-03-30 12:13:58 +0200

    feat(core): add support for Amazon

--- enabledSites.js
+++ enabledSites.js
@@ -1,3 +1,4 @@
 globalThis.enabledSites = {
-    'https?:\/\/(www\.)?youtube\.com/(.*)?': 'input#search'
+    'https?:\/\/(www\.)?youtube\.com/(.*)?': 'input#search',
+    'https?:\/\/(www\.)?amazon\.[a-z]{1,3}\/(.*)?': 'input#twotabsearchtextbox'
 };
